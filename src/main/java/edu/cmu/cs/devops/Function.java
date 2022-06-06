83879d4724d3c85fa8929dc74865f6cf23f63de7
OyseenW <1371546789@qq.com>
2022-06-06 18:14:26 -0400

    uodate

--- src/main/java/edu/cmu/cs/devops/Function.java
+++ src/main/java/edu/cmu/cs/devops/Function.java
@@ -3,6 +3,7 @@ package edu.cmu.cs.devops;
 import java.util.*;
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
+import org.apache.commons.codec.binary.Base64;
 
 /**
  * Azure Functions with HTTP Trigger.
@@ -26,7 +27,46 @@ public class Function {
         if (name == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+            org.apache.commons.codec.binary.Base64 base64 = new Base64();
+            byte[] decodedContextBytes = base64.decodeBase64(name);
+            String decodedContext = new String(decodedContextBytes);
+            // System.out.println(decodedContext);
+            String[] words = decodedContext.split(" ");
+            for (String w: words) {
+                // System.out.print(w + " ");
+            }
+            // System.out.println(" ");
+            LinkedList<String> sortedWords = new LinkedList<>();
+            sortedWords.add(words[0]);
+            for (int j = 1; j < words.length; j++) {
+                boolean flag = false;
+                for (int i = 0; i < sortedWords.size(); i++) {
+                    if (words[j].compareToIgnoreCase(sortedWords.get(i)) < 0) {
+                        sortedWords.add(i, words[j]);
+                        break;
+                    }
+                    if (i == sortedWords.size() - 1) {
+                        flag = true;
+                    }
+                }
+                if (flag) {
+                    sortedWords.addLast(words[j]);
+                }
+            }
+            String result = "";
+            for (String w: sortedWords) {
+                result += w;
+                result += " ";
+            }
+            result = result.trim();
+            // System.out.println(result);
+
+            String encodeResult = base64.encodeBase64String(result.getBytes());
+
+             System.out.println(encodeResult);
+
+
+            return request.createResponseBuilder(HttpStatus.OK).body(encodeResult).build();
         }
     }
 }
